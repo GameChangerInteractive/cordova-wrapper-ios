93a45025c9ec9139b30ec56f934f168908aa8f58
Nikolay Glushchenko <nick@nickalie.com>
2018-10-31 18:15:58 +0300

    integrated geolocation plugin

--- GCMVP/www/cordova_plugins.js
+++ GCMVP/www/cordova_plugins.js
@@ -322,6 +322,38 @@ module.exports = [
       "clobbers": [
                    "FirebasePlugin"
        ]
+  },
+  {
+  "id": "cordova-plugin-geolocation.Coordinates",
+  "file": "plugins/cordova-plugin-geolocation/www/Coordinates.js",
+  "pluginId": "cordova-plugin-geolocation",
+  "clobbers": [
+               "Coordinates"
+               ]
+  },
+  {
+  "id": "cordova-plugin-geolocation.PositionError",
+  "file": "plugins/cordova-plugin-geolocation/www/PositionError.js",
+  "pluginId": "cordova-plugin-geolocation",
+  "clobbers": [
+               "PositionError"
+               ]
+  },
+  {
+  "id": "cordova-plugin-geolocation.Position",
+  "file": "plugins/cordova-plugin-geolocation/www/Position.js",
+  "pluginId": "cordova-plugin-geolocation",
+  "clobbers": [
+               "Position"
+               ]
+  },
+  {
+  "id": "cordova-plugin-geolocation.geolocation",
+  "file": "plugins/cordova-plugin-geolocation/www/geolocation.js",
+  "pluginId": "cordova-plugin-geolocation",
+  "clobbers": [
+               "navigator.geolocation"
+               ]
   }
 ];
 module.exports.metadata = 
